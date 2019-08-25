8786ca9cb83415a43f9182820a972c55f9a44e79
katelynrm <katelynrm@hotmail.com>
2019-08-25 12:44:14 -0600

    additional functions

--- app.py
+++ app.py
@@ -9,14 +9,12 @@ import os
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'planets.db')
 app.config['JWT_SECRET_KEY'] = 'super-secret'
 app.config['MAIL_SERVER'] = 'smtp.mailtrap.io'
-# app.config['MAIL_PORT'] = 2525
 app.config['MAIL_USERNAME'] = os.environ['MAIL_USERNAME']
 app.config['MAIL_PASSWORD'] = os.environ['MAIL_PASSWORD']
-# app.config['MAIL_USE_TLS'] = True
-# app.config['MAIL_USE_SSL'] = False
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
@@ -159,9 +157,72 @@ def retrieve_password(email: str):
         return jsonify(message='That email doesnt exist'), 401
 
 
-@app.route('/planet_details/<int: planet_id>', methods=['GET'])
+@app.route('/planet_details/<int:planet_id>', methods=['GET'])
 def planet_details(planet_id: int):
-    
+    planet = Planet.query.filter_by(planet_id=planet_id).first()
+    if planet:
+        result = planet_schema.dump(planet)
+        return jsonify(result)
+    else:
+        return jsonify(message='That planet does not exist'), 404
+
+
+@app.route('/add_planet', methods=['POST'])
+@jwt_required
+def add_planet():
+    planet_name = request.form['planet_name']
+    test = Planet.query.filter_by(planet_name=planet_name).first()
+    if test:
+        return jsonify(message='There is already a planet by that name'), 409
+    else:
+        planet_type = request.form['planet_type']
+        home_star = request.form['home_star']
+        mass = float(request.form['mass'])
+        radius = float(request.form['radius'])
+        distance = float(request.form['distance'])
+
+        new_planet = Planet(planet_name=planet_name,
+                            planet_type=planet_type,
+                            home_star=home_star,
+                            mass=mass,
+                            radius=radius,
+                            distance=distance)
+
+        db.session.add(new_planet)
+        db.session.commit() 
+        return jsonify(message='A new planet was added'), 201
+
+
+@app.route('/update_planet', methods=['PUT'])
+@jwt_required
+def update_planet():
+    planet_id = int(request.form['planet_id'])
+    planet = Planet.query.filter_by(planet_id=planet_id).first()
+    if planet:
+        planet.planet_name = request.form['planet_name']
+        planet.planet_type = request.form['planet_type']
+        planet.home_star = request.form['home_star']
+        planet.mass = float(request.form['mass'])
+        planet.radius = float(request.form['radius'])
+        planet.distance = float(request.form['distance'])
+        db.session.commit()
+        return jsonify(message='Planet is updated'), 202
+    else:
+        return jsonify(message='Planet does not exist'), 404
+
+
+@app.route('/delete_planet/<int:planet_id>', methods=['DELETE'])
+@jwt_required
+def delete_planet(planet_id: int):
+    planet = Planet.query.filter_by(planet_id=planet_id).first()
+    if planet:
+        db.session.delete(planet)
+        db.session.commit()
+        return jsonify(message='Planet deleted'), 202
+    else:
+        return jsonify(message='Planet does not exist'), 404
+
+
 
 # database models
 class User(db.Model):
